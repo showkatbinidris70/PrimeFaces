f68f28f613ab8876b109946a2677d6dd79d9649e
Sharif Hossain <mamundndc@gmail.com>
2019-05-15 16:36:25 +0600

    a

---
+++ Ch5/src/java/com/coderbd/Bus.java
@@ -0,0 +1,53 @@
+
+package com.coderbd;
+
+
+public class Bus {
+    private String id;
+    private String year;
+    private String brand;
+    private String color;
+
+    public Bus() {
+    }
+
+    public Bus(String id, String year, String brand, String color) {
+        this.id = id;
+        this.year = year;
+        this.brand = brand;
+        this.color = color;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+    
+}
---
+++ Ch5/src/java/com/coderbd/BusController.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.coderbd;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+
+/**
+ *
+ * @author User
+ */
+@ManagedBean
+public class BusController {
+
+    private List<Bus> busList;
+    private List<Bus> busDetails;
+
+    public List<Bus> getBusList() {
+        busList = new ArrayList<>();
+        busList.add(new Bus("100", "1980", "Toyota", "Red"));
+        busList.add(new Bus("101", "1981", "Toyota", "Green"));
+        busList.add(new Bus("102", "1982", "Toyota", "Yellow"));
+        busList.add(new Bus("103", "1983", "Toyota", "Red"));
+        busList.add(new Bus("104", "1984", "Toyota", "Orange"));
+        busList.add(new Bus("105", "1985", "Toyota", "Red"));
+        busList.add(new Bus("106", "1986", "Toyota", "Black"));
+        busList.add(new Bus("107", "1987", "Toyota", "Black"));
+        return busList;
+    }
+
+    
+    public void setBusList(List<Bus> busList) {
+        this.busList = busList;
+    }
+
+    public List<Bus> getBusDetails() {
+        return busDetails;
+    }
+
+    public void setBusDetails(List<Bus> busDetails) {
+        this.busDetails = busDetails;
+    }
+
+    
+}
